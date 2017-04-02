9bd6252c18afd3deab61db6e840587f114ee7beb
Ryuji Utsunomiya <r.utsunomiya63@gmail.com>
2017-04-02 18:55:05 +0900

    更新

--- ask_form.php
+++ ask_form.php
@@ -16,7 +16,7 @@
     <form method="post" action="insert2.php" enctype="multipart/form-data">
         <div class="jumbotron">
     <legend></legend>
-     <label>質問のタイトル（50文字以内）：<input type="title" name="title"></label><br>
+     <label>質問のタイトル（40文字以内）：<input type="title" name="title"></label><br>
      <label>質問内容：<input type="textarea" name="question"></label><br>
      <label>回答者の選択（今は実装しない）：<input type="text" name="select"></label><br>
      <label><input type="hidden" name="id" value="<?=$id?>"></label><br>
